9e5591aca8c2e66500f1d921f4c0282b87f0ea0b
John Sedlak <john.sedlak@outlook.com>
2015-10-18 22:31:59 -0400

    Adds code

---
+++ src/JSSC.SimpleHash/Hasher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace JSSC.SimpleHash
+{
+    public static class Hasher
+    {
+        public static string GeneratePassword(string input, ref string salt, Encoding encoding = null)
+        {
+            return Md5Hash(Sha1Hash(input, ref salt, encoding), ref salt, encoding);
+        }
+
+        /// <summary>
+        /// Creates an MD5 hash of a string using a salt. If no salt is specified, one is created.
+        /// </summary>
+        /// <param name="input">The plain text input to hash.</param>
+        /// <param name="salt">The salt used to create hashed string.</param>
+        /// <param name="encoding">The encoding used to gather the bytes. Defaults to UTF8.</param>
+        /// <returns>A hashed version of the input string.</returns>
+        public static string Md5Hash(string input, ref string salt, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = System.Text.Encoding.UTF8;
+
+            if (string.IsNullOrWhiteSpace(salt))
+                salt = KeyGen.Generate(15, KeyGen.AllCharacters);
+
+            byte[]
+                saltBytes = encoding.GetBytes(salt),
+                inputBytes = encoding.GetBytes(input);
+
+            var hmacMd5 = new HMACMD5(saltBytes);
+
+            byte[] hash = hmacMd5.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(hash); //encoding.GetString(hash);
+        }
+
+        /// <summary>
+        /// Creates a SHA1 hash of a string using a salt. If no salt is specified, one is created.
+        /// </summary>
+        /// <param name="input">The plain text input to hash.</param>
+        /// <param name="salt">The salt used to create hashed string.</param>
+        /// <param name="encoding">The encoding used to gather the bytes. Defaults to UTF8.</param>
+        /// <returns>A hashed version of the input string.</returns>
+        public static string Sha1Hash(string input, ref string salt, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = System.Text.Encoding.UTF8;
+
+            if (string.IsNullOrWhiteSpace(salt))
+                salt = KeyGen.Generate(15, KeyGen.AllCharacters);
+
+            byte[]
+                saltBytes = encoding.GetBytes(salt),
+                inputBytes = encoding.GetBytes(input);
+
+            var hmacSha1 = new HMACSHA1(saltBytes);
+
+            byte[] hash = hmacSha1.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(hash);// encoding.GetString(hash);
+        }
+    }
+}
---
+++ src/JSSC.SimpleHash/KeyGen.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace JSSC.SimpleHash
+{
+    public static class KeyGen
+    {
+        public const string AllCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        public const string UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        public const string LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+        /// <summary>
+        /// Generate a crypto-random string of specified length
+        /// </summary>
+        /// <param name="size">The length of the string</param>
+        /// <param name="allowedCharacters">A string containing the allowed characters</param>
+        /// <returns>A cryptographic random string of specified length</returns>
+        public static string Generate(int size = 15, string allowedCharacters = AllCharacters)
+        {
+            char[] chars = allowedCharacters.ToCharArray();
+            byte[] data = new byte[1];
+
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+
+            StringBuilder result = new StringBuilder(size);
+
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length - 1)]);
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Generate a crypto-random string of specified length with dashes every 5 characters (for size > 10)
+        /// </summary>
+        /// <param name="size">The length of the string</param>
+        /// <param name="allowedCharacters">A string containing the allowed characters</param>
+        /// <returns>A cryptographic random string of specified length</returns>
+        public static string GenerateKey(int size = 15, string allowedCharacters = AllCharacters)
+        {
+            string rawKey = Generate(size, allowedCharacters);
+
+            if (size <= 10) return rawKey;
+
+            for (int i = size - 5; i > 0; i -= 5)
+                rawKey = rawKey.Insert(i, "-");
+
+            return rawKey;
+        }
+    }
+}
